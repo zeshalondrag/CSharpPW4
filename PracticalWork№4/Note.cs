b7d5e81232ff6da73091fa9b235561a9e804fd48
zeshalondrag <144255214+zeshalondrag@users.noreply.github.com>
2023-12-04 13:21:28 +0300

    Note.cs

--- PracticalWork№4/Note.cs
+++ PracticalWork№4/Note.cs
@@ -1,1 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace CSharpPW4
+{
+    internal class Note
+    {
+        private string name, description;
+        private DateTime date;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+    }
+}
